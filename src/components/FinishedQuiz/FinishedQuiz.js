14685cc03052cd3c0b9c03e0f084796edbc3c062
artem <mstrJesm@gmail.com>
2019-02-21 23:07:45 +0200

    add new elements in FinishedQuiz.js

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -7,8 +7,13 @@ const FinishedQuiz = () => {
       <ul>
         <li>
           <strong>1. How are you?</strong>
+          <i className="fa" />
         </li>
       </ul>
+      <p>Right 4 of 10</p>
+      <div>
+        <button>Repeat</button>
+      </div>
     </div>
   );
 };
